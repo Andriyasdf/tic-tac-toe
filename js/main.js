8314cd3556ae778f51891daf4550284eb7119233
Andriy Massimilla <andriymassimilla@gmail.com>
2017-02-22 23:08:24 -0500

    Store cell data in array
    Draw cell data to DOM independently

--- js/main.js
+++ js/main.js
@@ -1,54 +1,87 @@
 var game = {
 	turn: 0,
-	players: {
-		0: {
-			name: "John"
+	player: {
+		x: {
+			name: "Orange"
 		},
-		1: {
-			name: "Billy"
+		o: {
+			name: "Blue"
 		}
 	},
-	cells: [
-		[0, 0, 0],
-		[0, 0, 0],
-		[0, 0, 0]
+	grid: [
+		"", "", "",
+		"", "", "",
+		"", "", ""
 	]
 };
+var cellState = {
+	NONE: "",
+	X: "x",
+	O: "o"
+};
+
 
 // Check if grid cell was clicked
 $(".row div").click(function(event) {
-	if (game.turn % 2 == 1) {
-		setCell(event.target, "x");
+	if (game.turn % 2 == 0) {
+		setCell(event.target, cellState.X);
 	} else {
-		setCell(event.target, "o");
+		setCell(event.target, cellState.O);
 	}
 });
 
-
+// Reset the board, but save the player setup
 function newGame() {
 	// Reset the cells
-	for (var i = 0; i < 9; ++i) {
-		$(".row div").removeClass("x o");
+	for (var i = 0; i < game.grid.length; ++i) {
+		$("#" + i).removeClass("x o");
+		$("#" + i).addClass(game.grid[i]);
 	}
-	console.log("New game initialized");
+
+	game.turn = 0;
+	game.grid = [
+		"", "", "",
+		"", "", "",
+		"", "", ""
+	];
+}
+
+// Reset everything
+function resetGame() {
+	newGame();
+
+	game.player.x.name = prompt("Player 1's name:", game.player.x.name);
+	game.player.o.name = prompt("Player 2's name:", game.player.o.name);
 }
 
 function setCell(cell, value) {
+	// Make sure cell is clear
 	if ($(cell).hasClass("x") || $(cell).hasClass("o")) {
-		console.log("Cell already assigned!");
 		return false;
 	}
 
-	$(cell).removeClass("x o");
-	$(cell).addClass(value);
+	// Set the value in the grid array
+	game.grid[$(cell).attr("id")] = value;
 
+	// Check if in winning state
+	// NOTE: we can use magic squares for this :)
+	if (game.grid[0] == game.grid[1] == game.grid[2] == cellState.X) {
+		console.log("X wins!");
+	}
 
+	console.log(game.grid);
 	game.turn++;
 }
 
 function draw() {
-	$("#current_player").html(game.turn % 2 ? "<b><span class='x'><i class='fa fa-times'></i></span>'s turn</b>" : "<b><span class='o'><i class='fa fa-circle'></i></span>'s turn</b>");
+	// Draw the grid
+	for (var j = 0; j < game.grid.length; ++j) {
+		$("#" + j).removeClass("x o");
+		$("#" + j).addClass(game.grid[j]);
+	}
+
+	$("#players").html("<span class='x'>" + game.player.x.name + "</span> vs <span class='o'>" + game.player.o.name + "</b>");
 	$("#turn_count").html("<b>turn - </b>" + game.turn);
-	$("#players").html("<b>" + game.players[0].name + "</b> vs <b>" + game.players[1].name + "</b>");
+	$("#current_player").html(game.turn % 2 == 0 ? "<span class='x'>" + game.player.x.name + "'s turn</span>" : "<span class='o'>" + game.player.o.name + "'s turn</span>");
 }
-setInterval(draw, 16);
+setInterval(draw, 20);
