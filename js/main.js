fdc9ecc3f29041549e1608714d6b3d5649293fd6
Andriy Massimilla <andriymassimilla@gmail.com>
2017-02-22 20:56:08 -0500

    Control panel for resetting game

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ var game = {
 		1: {
 			name: "Billy"
 		}
-	}
+	},
+	cells: [
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 0]
+	]
 };
 
 // Check if grid cell was clicked
@@ -19,8 +24,14 @@ $(".row div").click(function(event) {
 	}
 });
 
-setInterval(draw, 16);
 
+function newGame() {
+	// Reset the cells
+	for (var i = 0; i < 9; ++i) {
+		$(".row div").removeClass("x o");
+	}
+	console.log("New game initialized");
+}
 
 function setCell(cell, value) {
 	if ($(cell).hasClass("x") || $(cell).hasClass("o")) {
@@ -30,10 +41,14 @@ function setCell(cell, value) {
 
 	$(cell).removeClass("x o");
 	$(cell).addClass(value);
+
+
 	game.turn++;
 }
 
 function draw() {
 	$("#current_player").html(game.turn % 2 ? "<b><span class='x'><i class='fa fa-times'></i></span>'s turn</b>" : "<b><span class='o'><i class='fa fa-circle'></i></span>'s turn</b>");
 	$("#turn_count").html("<b>turn - </b>" + game.turn);
+	$("#players").html("<b>" + game.players[0].name + "</b> vs <b>" + game.players[1].name + "</b>");
 }
+setInterval(draw, 16);
