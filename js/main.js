5c5b97fd9a4b178d9bc0cd47049dea46a8e61487
Andriy Massimilla <andriymassimilla@gmail.com>
2017-02-23 19:19:22 -0500

    Check for winning states
    This is very temporary (not optimized at all, just a placeholder)

--- js/main.js
+++ js/main.js
@@ -2,17 +2,15 @@ var game = {
 	turn: 0,
 	player: {
 		x: {
-			name: "Orange"
+			name: "Orange",
+			wins: 0
 		},
 		o: {
-			name: "Blue"
+			name: "Blue",
+			wins: 0
 		}
 	},
-	grid: [
-		"", "", "",
-		"", "", "",
-		"", "", ""
-	]
+	grid: []
 };
 var cellState = {
 	NONE: "",
@@ -20,58 +18,78 @@ var cellState = {
 	O: "o"
 };
 
+// Set up the game
+resetGrid();
+
 
 // Check if grid cell was clicked
 $(".row div").click(function(event) {
-	if (game.turn % 2 == 0) {
-		setCell(event.target, cellState.X);
-	} else {
-		setCell(event.target, cellState.O);
-	}
+	game.turn % 2 == 0 ? setCell($(event.target).attr("id"), cellState.X) : setCell($(event.target).attr("id"), cellState.O);
 });
-
-// Reset the board, but save the player setup
-function newGame() {
-	// Reset the cells
-	for (var i = 0; i < game.grid.length; ++i) {
-		$("#" + i).removeClass("x o");
-		$("#" + i).addClass(game.grid[i]);
-	}
-
-	game.turn = 0;
-	game.grid = [
-		"", "", "",
-		"", "", "",
-		"", "", ""
-	];
-}
-
-// Reset everything
-function resetGame() {
-	newGame();
-
-	game.player.x.name = prompt("Player 1's name:", game.player.x.name);
-	game.player.o.name = prompt("Player 2's name:", game.player.o.name);
-}
 
 function setCell(cell, value) {
 	if (!document.getElementById("overwrite_mode").checked) {
 		// Make sure cell is clear
-		if ($(cell).hasClass("x") || $(cell).hasClass("o")) {
+		if (game.grid[cell] == cellState.X || game.grid[cell] == cellState.O) {
 			return false;
 		}
 	}
 
 	// Set the value in the grid array
-	game.grid[$(cell).attr("id")] = value;
+	game.grid[cell] = value;
 
 	// Check if in winning state
-	// NOTE: we can use magic squares for this :)
-	if (game.grid[0] == game.grid[1] == game.grid[2] == cellState.X) {
-		console.log("X wins!");
+	// TODO crude, please update ASAP
+	if (
+		game.grid[0] + game.grid[1] + game.grid[2] == "xxx" ||
+		game.grid[3] + game.grid[4] + game.grid[5] == "xxx" ||
+		game.grid[6] + game.grid[7] + game.grid[8] == "xxx" ||
+		game.grid[0] + game.grid[3] + game.grid[6] == "xxx" ||
+		game.grid[1] + game.grid[4] + game.grid[7] == "xxx" ||
+		game.grid[2] + game.grid[5] + game.grid[8] == "xxx" ||
+		game.grid[0] + game.grid[4] + game.grid[8] == "xxx" ||
+		game.grid[2] + game.grid[4] + game.grid[8] == "xxx"
+	) {
+		winGame(game.player.x);
+	} else if (
+		game.grid[0] + game.grid[1] + game.grid[2] == "ooo" ||
+		game.grid[3] + game.grid[4] + game.grid[5] == "ooo" ||
+		game.grid[6] + game.grid[7] + game.grid[8] == "ooo" ||
+		game.grid[0] + game.grid[3] + game.grid[6] == "ooo" ||
+		game.grid[1] + game.grid[4] + game.grid[7] == "ooo" ||
+		game.grid[2] + game.grid[5] + game.grid[8] == "ooo" ||
+		game.grid[0] + game.grid[4] + game.grid[8] == "ooo" ||
+		game.grid[2] + game.grid[4] + game.grid[8] == "ooo"
+	) {
+		winGame(game.player.o);
 	}
 
 	game.turn++;
+}
+
+function winGame(player) {
+	player.wins++;
+	resetGrid();
+	console.log(player.name + " wins!");
+}
+
+function resetGame() {
+	var x_name = prompt("X's name:", game.player.x.name);
+	var o_name = prompt("O's name:", game.player.o.name);
+
+	game.player.x.name = x_name;
+	game.player.o.name = x_name;
+
+	resetGrid();
+}
+
+function resetGrid() {
+	game.turn = 0;
+	game.grid = [
+		cellState.NONE, cellState.NONE, cellState.NONE,
+		cellState.NONE, cellState.NONE, cellState.NONE,
+		cellState.NONE, cellState.NONE, cellState.NONE
+	];
 }
 
 function draw() {
@@ -81,8 +99,8 @@ function draw() {
 		$("#" + j).addClass(game.grid[j]);
 	}
 
-	$("#players").html("<span class='x'>" + game.player.x.name + "</span> vs <span class='o'>" + game.player.o.name + "</b>");
-	$("#turn_count").html("<b>turn - </b>" + game.turn);
-	$("#current_player").html(game.turn % 2 == 0 ? "<span class='x'>" + game.player.x.name + "'s turn</span>" : "<span class='o'>" + game.player.o.name + "'s turn</span>");
+	$("#players").html("<span class='x'>" + game.player.x.name + "(" + game.player.x.wins + ")" + "</span> vs <span class='o'>" + game.player.o.name + "(" + game.player.o.wins + ")" + "</span>");
+	$("#turn_count").html("<b>turn: </b>" + game.turn);
+	$("#current_player").html((game.turn % 2 == 0 ? "<span class='x'>" + game.player.x.name : "<span class='o'>" + game.player.o.name) + "</span>'s turn");
 }
 setInterval(draw, 20);
