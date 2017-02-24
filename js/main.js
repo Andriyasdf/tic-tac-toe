b1833bcec320de1dd1e9b47edb11b06c79045d10
Andriy Massimilla <andriymassimilla@gmail.com>
2017-02-23 22:06:16 -0500

    Basic CPU opponent

--- js/main.js
+++ js/main.js
@@ -21,20 +21,43 @@ var cellState = {
 // Set up the game
 resetGrid();
 
-
 // Check if grid cell was clicked
 $(".row div").click(function(event) {
-	game.turn % 2 == 0 ? setCell($(event.target).attr("id"), cellState.X) : setCell($(event.target).attr("id"), cellState.O);
-});
+	var cell = $(event.target).attr("id");
 
-function setCell(cell, value) {
+	// Make sure cell is clear
 	if (!document.getElementById("overwrite_mode").checked) {
-		// Make sure cell is clear
-		if (game.grid[cell] == cellState.X || game.grid[cell] == cellState.O) {
+		if (isCellAssigned(cell)) {
+			console.warn("Cell is already assigned");
 			return false;
 		}
 	}
 
+	if (game.turn % 2 == 0) {
+		setCell(cell, cellState.X);
+	} else {
+		// Check if versus computer
+			cpuMove();
+		} else {
+			setCell(cell, cellState.O);
+		}
+	}
+});
+
+// Run computer opponent move
+function cpuMove() {
+	var randomCell = Math.floor(Math.random()*8);
+
+	// Select a random cell
+	if (!isCellAssigned(randomCell)) {
+		setCell(randomCell, cellState.O);
+	} else {
+		cpuMove();
+		console.warn("CPU tried placing in assigned cell");
+	}
+}
+
+function setCell(cell, value) {
 	// Set the value in the grid array
 	game.grid[cell] = value;
 
@@ -51,6 +74,7 @@ function setCell(cell, value) {
 		game.grid[2] + game.grid[4] + game.grid[8] == "xxx"
 	) {
 		winGame(game.player.x);
+		return;
 	} else if (
 		game.grid[0] + game.grid[1] + game.grid[2] == "ooo" ||
 		game.grid[3] + game.grid[4] + game.grid[5] == "ooo" ||
@@ -62,25 +86,45 @@ function setCell(cell, value) {
 		game.grid[2] + game.grid[4] + game.grid[8] == "ooo"
 	) {
 		winGame(game.player.o);
+		return;
+	}
+
+	// Tie condition
+	if (game.grid.every(function(cell) {
+		return cell == cellState.X || cell == cellState.O;
+	})) {
+		winGame();
+		return;
 	}
 
 	game.turn++;
 }
 
+function isCellAssigned(cell) {
+	return (game.grid[cell] == cellState.X || game.grid[cell] == cellState.O);
+}
+
 function winGame(player) {
-	player.wins++;
-	resetGrid();
-	console.log(player.name + " wins!");
+	if (player == null) {
+		resetGrid();
+		console.log("Nobody wins!");
+	} else {
+		player.wins++;
+		resetGrid();
+		console.log(player.name + " wins!");
+	}
 }
 
 function resetGame() {
+	resetGrid();
+
 	var x_name = prompt("X's name:", game.player.x.name);
 	var o_name = prompt("O's name:", game.player.o.name);
 
 	game.player.x.name = x_name;
 	game.player.o.name = o_name;
 
-	resetGrid();
+	}
 }
 
 function resetGrid() {
@@ -94,13 +138,13 @@ function resetGrid() {
 
 function draw() {
 	// Draw the grid
-	for (var j = 0; j < game.grid.length; ++j) {
-		$("#" + j).removeClass("x o");
-		$("#" + j).addClass(game.grid[j]);
-	}
+	game.grid.forEach(function(value, cell) {
+		$("#" + cell).removeClass("x o");
+		$("#" + cell).addClass(game.grid[cell]);
+	});
 
+	$("#turn_count").html("turn <b>" + game.turn + "</b>");
 	$("#players").html("<span class='x'>" + game.player.x.name + "(" + game.player.x.wins + ")" + "</span> vs <span class='o'>" + game.player.o.name + "(" + game.player.o.wins + ")" + "</span>");
-	$("#turn_count").html("<b>turn: </b>" + game.turn);
 	$("#current_player").html((game.turn % 2 == 0 ? "<span class='x'>" + game.player.x.name : "<span class='o'>" + game.player.o.name) + "</span>'s turn");
 }
 setInterval(draw, 20);
